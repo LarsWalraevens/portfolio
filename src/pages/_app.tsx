fb36d191b5ee0735ccb5dd945f4e8facf31ff762
Lars Walraevens <larswalraevens@live.be>
2023-04-03 22:50:53 +0200

    load overlay delay

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -37,7 +37,7 @@ export default function App({ Component, pageProps }: AppProps) {
     if (appStore.disabledFade === false) {
       setTimeout(() => {
         $("#load-overlay").hide();
-      }, 70);
+      }, 400);
 
     } else {
       $("#load-overlay").hide();
